6cb989eba4e3431f61d4bfdf1dc2da5404e43782
GawronDev <kubusgawron@gmail.com>
2021-05-23 21:05:39 +0200

    Kolorki

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivymd.app import MDApp
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ObjectProperty
+from kivy.properties import NumericProperty, ListProperty
 from kivy.uix.button import Button
 from kivy.animation import Animation
 from kivymd.uix.label import MDLabel
@@ -16,7 +16,7 @@ class AngleLabel(MDLabel):
 
 
 class RadarDot(Widget):
-    random_color = [random.random(), random.random(), random.random(), 1]
+    random_color = ListProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -51,7 +51,7 @@ class RotatingLineWidget(Widget):
         super().__init__(**kwargs)
 
         try:
-            self.myradar = Radar('/dev/tty.usbmodem14202')
+            self.myradar = Radar('COM3')
 
         except Exception:
             pass
@@ -96,12 +96,25 @@ class RotatingLineWidget(Widget):
 
         if dot_distance != 0:
             x = dot_distance * math.cos(math.radians(dot_angle))
-            print(x)
+            # print(x)
             y = dot_distance * math.sin(math.radians(dot_angle))
-            print(y)
+            # print(y)
 
             # print(str(x), str(y))
-            self.main_float.add_widget(RadarDot(pos=(x, y)))
+
+            if dot_distance <= 100:
+                color = [dot_distance/100, 0, 0, 1]
+
+            elif 100 < dot_distance <= 200:
+                color = [0, dot_distance/200, 1, 1]
+
+            elif 200 < dot_distance <= 300:
+                color = [0, dot_distance/300, 0, 1]
+
+            else:
+                color = [0, 0, dot_distance/400, 1]
+
+            self.main_float.add_widget(RadarDot(pos=(x, y), random_color=color))
 
         # print(dot_angle)
 
